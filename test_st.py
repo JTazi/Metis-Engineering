dd3cecfe1c6ced606bbe482d1d73e4503b1260de
JTazi <99365644+JTazi@users.noreply.github.com>
2022-10-05 10:46:34 -0500

    Update test_st.py

--- test_st.py
+++ test_st.py
@@ -16,7 +16,8 @@ keyword_url = 'https://en.wikipedia.org/wiki/List_of_Magic:_The_Gathering_keywor
 
 #streamlit page config
 st.set_page_config(
-    layout="centered", page_icon='https://github.com/JTazi/Metis-Engineering/blob/901e6b1f820c12b29199ef635e835f4f2d395280/kisspng-magic-the-gathering-duels-of-the-planeswalker-magic-the-gathering-commander-5b1c8faf3cc9e6.955806671528598447249.png', page_title="MTG Table App"
+	#'centered'
+    	layout="wide", page_icon='https://github.com/JTazi/Metis-Engineering/blob/901e6b1f820c12b29199ef635e835f4f2d395280/kisspng-magic-the-gathering-duels-of-the-planeswalker-magic-the-gathering-commander-5b1c8faf3cc9e6.955806671528598447249.png', page_title="MTG Table App"
 )
 st.title("Magic the Gathering Card Browser")
 st.write(
